cbf8616e8b85249f81faa4fb0f3005f75ec2263c
SkyWex <vllad.soltan@gmail.com>
2018-10-10 17:21:40 +0300

    added auto-generating of signed apk;

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # PPO app
-v.0.0.5
+v.0.0.6
 
 + created an empty template Andoid 6.0+ application;
 + added runtime-permissions;
 + IMEI displaying;
 + SEMVER support;
 + project version displaying;
++ generating of signed apk;
--- app/src/main/java/com/skywex/ppoapp/MainActivity.java
+++ app/src/main/java/com/skywex/ppoapp/MainActivity.java
@@ -1,27 +1,19 @@
 package com.skywex.ppoapp;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import static android.Manifest.permission.ACCESS_FINE_LOCATION;
-import static android.Manifest.permission.CAMERA;
 import static android.Manifest.permission.READ_PHONE_STATE;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -32,26 +24,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                mTextMessage.setText(R.string.title_home);
+                return true;
+            case R.id.navigation_dashboard:
+                mTextMessage.setText(R.string.title_dashboard);
+                return true;
+            case R.id.navigation_notifications:
+                mTextMessage.setText(R.string.title_notifications);
+                return true;
+        }
+        return false;
         }
     };
 
     public String getIMEI(Context context) {
-        TelephonyManager mngr = (TelephonyManager) context.getSystemService(context.TELEPHONY_SERVICE);
-        String imei = mngr.getDeviceId();
+        TelephonyManager manager = (TelephonyManager) context.getSystemService(context.TELEPHONY_SERVICE);
+        Button request_permission = findViewById(R.id.request_permission);
 
-        return imei;
+        if (!checkPermission()) {
+            request_permission.setVisibility(View.VISIBLE);
+            request_permission.setOnClickListener(this);
+            Snackbar.make(request_permission, "Please, give permission to access your IMEI.", Snackbar.LENGTH_LONG).show();
+
+            return "none";
+        } else return manager.getDeviceId();
+    }
+
+    public String getVersion() {
+        PackageInfo pInfo = null;
+        try {
+            pInfo = this.getPackageManager().getPackageInfo(getPackageName(), 0);
+
+            return pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+
+            return "none";
+        }
     }
 
     @Override
@@ -59,31 +70,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        TextView imei_value = (TextView) findViewById(R.id.imei_value);
-        TextView version_value = (TextView) findViewById(R.id.version_value);
-        Button request_permission = (Button) findViewById(R.id.request_permission);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-
-        request_permission.setOnClickListener(this);
+        mTextMessage = findViewById(R.id.message);
+        TextView imei_value = findViewById(R.id.imei_value);
+        TextView version_value = findViewById(R.id.version_value);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        PackageInfo pInfo = null;
-        try {
-            pInfo = this.getPackageManager().getPackageInfo(getPackageName(), 0);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        String version = pInfo.versionName;
-        version_value.setText("Версия: " + version);
+        imei_value.setText("IMEI: " + getIMEI(this));
+        imei_value.setVisibility(View.VISIBLE);
 
-        if (checkPermission()) {
-            imei_value.setText("IMEI: " + getIMEI(this));
-            imei_value.setVisibility(View.VISIBLE);
-        } else {
-            request_permission.setVisibility(View.VISIBLE);
-            Snackbar.make(navigation, "Please, give application permission to access your IMEI.", Snackbar.LENGTH_LONG).show();
-        }
+        version_value.setText("Версия: " + getVersion());
     }
 
     @Override
@@ -93,21 +89,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (id) {
             case R.id.request_permission:
-                if (!checkPermission())
-                    requestPermission();
-                else {
-                    TextView imei_value = (TextView) findViewById(R.id.imei_value);
-                    imei_value.setText("IMEI: " + getIMEI(this));
-                    imei_value.setVisibility(View.VISIBLE);
-                }
+                requestPermission();
                 break;
         }
     }
 
     private boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(getApplicationContext(), READ_PHONE_STATE);
-
-        return result == PackageManager.PERMISSION_GRANTED;
+        return  ActivityCompat.checkSelfPermission(getApplicationContext(), READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED;
     }
 
     private void requestPermission() {
@@ -119,19 +107,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (requestCode) {
             case PERMISSION_REQUEST_CODE:
                 if (grantResults.length > 0) {
-
-                    boolean phoneAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-
-                    if (phoneAccepted) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                         Button request_permission = findViewById(R.id.request_permission);
                         request_permission.setVisibility(View.GONE);
                         TextView imei_value = (TextView) findViewById(R.id.imei_value);
                         imei_value.setText("IMEI: " + getIMEI(this));
                         imei_value.setVisibility(View.VISIBLE);
                         Snackbar.make(view, "Now you can see your device IMEI.", Snackbar.LENGTH_LONG).show();
-                    } else {
+                    } else
                         Snackbar.make(view, "Permission denied, you cannot access your device IMEI.", Snackbar.LENGTH_LONG).show();
-                    }
                 }
 
                 break;
